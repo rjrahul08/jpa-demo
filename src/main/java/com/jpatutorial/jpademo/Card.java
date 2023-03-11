f9ff76fee0bec5b17c283737896a7909ca9bef7f
rjrahul08 <61372250+rjrahul08@users.noreply.github.com>
2023-03-12 00:15:32 +0530

    update1

---
+++ src/main/java/com/jpatutorial/jpademo/Card.java
@@ -0,0 +1,51 @@
+package com.jpatutorial.jpademo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cardNo;
+
+    private String name;
+
+    private String dob;
+
+    public Card(){
+
+    }
+
+    public Card(int cardNo, String name, String dob) {
+        this.cardNo = cardNo;
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public int getCardNo() {
+        return cardNo;
+    }
+
+    public void setCardNo(int cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
